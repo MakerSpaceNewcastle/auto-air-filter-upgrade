911b641beb69b66629171bc444b0ba4a8f9d4597
Dan Nixon <dan@dan-nixon.com>
2024-12-14 20:25:39 +0000

    wip

--- control/src/main.rs
+++ control/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+use rumqttc::{AsyncClient, MqttOptions, QoS};
+
+#[tokio::main]
+async fn main() {
+    let mut mqttoptions = MqttOptions::new("rumqtt-doot-test", "100.92.108.55", 1883);
+    mqttoptions.set_credentials("dan", "");
+    mqttoptions.set_keep_alive(Duration::from_secs(5));
+
+    let (mut client, mut connection) = AsyncClient::new(mqttoptions, 10);
+    client.subscribe("#", QoS::ExactlyOnce).await.unwrap();
+
+    loop {
+    let fuck = connection.poll().await;
+        println!("{:#?}", fuck);
+    }
 }
